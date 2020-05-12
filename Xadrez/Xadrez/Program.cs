6cda044122836816f912da7fc9485e67de08b048
alanricardos <alan.ricardo.silva@hotmail.com>
2020-05-12 12:02:19 -0300

    First Commit

---
+++ Xadrez/Xadrez/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xadrez.Tabuleiro;
+
+namespace Xadrez
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Posicao P = new Posicao(3, 4);
+            Console.WriteLine("Posição: "+ P);    
+        }
+    }
+}
